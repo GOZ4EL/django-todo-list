8f20c4139c42038e610d63f141af3b914ce820d5
goz4el <hordemzerado@gmail.com>
2021-08-17 20:12:57 -0400

    Adding list items on the view

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import TodoListItem
 
 def todoappView(request):
-    return render(request, 'todolist.html')
+    all_todo_items = TodoListItem.objects.all()
+    return render(
+        request, 
+        'todolist.html', 
+        {'all_items': all_todo_items}
+    )
+
