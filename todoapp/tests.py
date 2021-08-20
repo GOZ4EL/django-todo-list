6c5e16d3aee6390fe702f17f3122edab1ec8eb0a
goz4el <hordemzerado@gmail.com>
2021-08-19 23:16:42 -0400

    Creating test case for adding a new todo item

--- todoapp/tests.py
+++ todoapp/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import TodoListItem
+from . models import TodoListItem
 
 class TodoAppView(TestCase):
     def test_view_todo_items_list_is_showed(self):
@@ -9,3 +9,8 @@ class TodoAppView(TestCase):
         response = self.client.get('/todoapp/')
         self.assertQuerysetEqual(response.context['all_items'], [todoitem])
 
+    def test_view_new_item_is_showed(self):
+        content = "Test Content"
+        self.client.post('/addTodoItem/', {'content': content})
+        response = self.client.get('/todoapp/')
+        self.assertContains(response, content)
