a31dd3515f233020002b4e0801da3f285089b291
goz4el <hordemzerado@gmail.com>
2021-08-20 21:20:18 -0400

    Documenting the tests.

--- todoapp/tests.py
+++ todoapp/tests.py
@@ -5,11 +5,16 @@ from . models import TodoListItem
 
 class TodoAppView(TestCase):
     def test_view_todo_items_list_is_showed(self):
+        """The items in the todo list items are showed on the main page."""
         todoitem = TodoListItem.objects.create(content="Test Item.")
         response = self.client.get('/todoapp/')
         self.assertQuerysetEqual(response.context['all_items'], [todoitem])
 
-    def test_view_new_item_is_showed(self):
+    def test_view_new_item_is_added(self):
+        """
+        A new item is added to the todo list item whenever a 
+        post request is made to the '/addTodoItem/' dir.
+        """
         content = "Test Content"
         self.client.post('/addTodoItem/', {'content': content})
         response = self.client.get('/todoapp/')
